105860cb63e523f31c42488f2cd1614920ad7ae2
Ritam Pal <ritamjunior26@gmail.com>
2024-09-19 22:49:15 +0530

    comit1

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,9 @@
 import { AuthPage } from "@refinedev/antd";
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
   return <AuthPage type="register" />;
 };
